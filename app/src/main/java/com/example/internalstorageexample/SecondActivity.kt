d1b08ff2c2f1abb4adbbd56734a093ab19647444
Abidah Nabilah <abdhnabilah@gmail.com>
2022-05-23 21:33:26 +0700

    Menyimpan data teks pada internal storage

---
+++ app/src/main/java/com/example/internalstorageexample/SecondActivity.kt
@@ -0,0 +1,34 @@
+package com.example.internalstorageexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_second.*
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+    override fun onResume() {
+        super.onResume()
+        loadData()
+    }
+    private fun loadData() {
+        val filename = "ourfile.txt"
+        Thread(Runnable {
+            val input = openFileInput(filename)
+            input.use {
+                var buffer = StringBuilder()
+                var bytes_read = input.read()
+                while (bytes_read != -1) {
+                    buffer.append(bytes_read.toChar())
+                    bytes_read = input.read()
+                }
+                runOnUiThread(Runnable {
+
+                    txtoutput.setText(buffer.toString())
+                })
+            }
+        }).start()
+    }
+}
